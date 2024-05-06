91a4525177b85e8379b1db9123ca2c89a8c1ba38
JeremiahUnderwood <44481415+JeremiahUnderwood@users.noreply.github.com>
2024-05-06 17:04:44 -0500

    Array class added

---
+++ AltCtrlTeam3/Assets/ArrayBehavior/Array.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Array : MonoBehaviour
+{
+
+    [Header("FOR DEBUGGING ONLY, please do not mess with this unless you know what you are doing.")]
+    [SerializeField] private int[] PlayingField = {
+        0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int GetValue(int x, int y)
+    {
+        return PlayingField[(x-1) + (7 - y) * 7];  //visually returns the x,y coordinates from the graph above
+    }
+
+    public void SetValue(int x, int y, int value)
+    {
+        PlayingField[(x - 1) + (7 - y) * 7] = value;
+    }
+
+    public int[] GetPlayingField()
+    {
+        return PlayingField;
+    }
+}
